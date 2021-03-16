a3ea128e6a36278cbaa6c24ae8be6d46164a7eb5
samsond <nosmaspoise@gmail.com>
2021-03-16 12:33:06 +0300

    add template

---
+++ src/main/java/com/maincoders/cakefactory/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.maincoders.cakefactory.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
